892d7fca55463cd8c99a3edf947105d3843c962a
Dylan Schmit <djs.bitcoin@gmail.com>
2020-08-20 06:28:30 +0000

    fix box search taking from header text

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
-document.getElementById("search-button").addEventListener("click", headerSearch);
+document.getElementById("search-button").addEventListener("click", boxSearch);
 if ((new URL(window.location.href)).searchParams.get("err")) {
   document.querySelector("#err-msg").innerHTML = (new URL(window.location.href)).searchParams.get("err");
 }
-document.getElementById("search-text").addEventListener("keyup", (k) => {if (k.key == "Enter") headerSearch()});
-async function headerSearch() {
+document.getElementById("search-text").addEventListener("keyup", (k) => {if (k.key == "Enter") boxSearch()});
+async function boxSearch() {
   window.location = window.location.origin + "/stats/" + document.getElementById("search-text").value;
 }
