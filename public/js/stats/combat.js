0fd75435e7ca872882228c912d39b888f2876a7a
cyandev <djs.bitcoin@gmail.com>
2021-06-13 16:12:44 -0500

    combat + slayer

--- public/js/stats/combat.js
+++ public/js/stats/combat.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars, no-undef */
+var statPipeline = Array.from({length:10},x=>[]);
+const ADDATIVE_STATS = 0;
 
 function loadCombat(profileData) {
     //find combat items
     let weapons = [];
     let talismans = [];
     for (let inventory of profileData.inventories) {
-        console.log(inventory.contents)
         for (let item of inventory.contents) {
             if (item.tags && item.tags.includes("WEAPON")) { //find weapons
                 let weapon = {...item};
@@ -19,7 +20,60 @@ function loadCombat(profileData) {
             }
         }
     }
-    console.log(weapons,talismans);
+
+    //filter active talismans
+    activeTalismans = {};
+    inactiveTalismans = [];
+    for (let talisman of talismans) {
+        if (!talismanClassifier[talisman.id]) {
+            activeTalismans[talisman.id] = talisman;
+        } else if (activeTalismans[talismanClassifier[talisman.id]]) {
+            if (rarityNum[talisman.rarity] > rarityNum[activeTalismans[talismanClassifier[talisman.id]].rarity]) {
+                inactiveTalismans.push(activeTalismans[talismanClassifier[talisman.id]]); //move old to inactive
+                activeTalismans[talismanClassifier[talisman.id]] = talisman; //set active to new
+                continue;
+            } 
+
+            //check if rarity ties
+            if ((rarityNum[talisman.rarity] < rarityNum[activeTalismans[talismanClassifier[talisman.id]].rarity]) || //dont allow lower rarity
+            (talisman.origin != "Talisman Bag" && activeTalismans[talismanClassifier[talisman.id]].origin == "Talisman Bag") || //prefer talisman bag
+            (talisman.origin != "Inventory" && activeTalismans[talismanClassifier[talisman.id]].origin == "Inventory")) { //prefer inventory
+                inactiveTalismans.push(talisman);
+                continue;
+            }
+
+            //it tied on rarity but made it through
+
+            inactiveTalismans.push(activeTalismans[talismanClassifier[talisman.id]]); //move old to inactive
+            activeTalismans[talismanClassifier[talisman.id]] = talisman; //set active to new
+        } else {
+            activeTalismans[talismanClassifier[talisman.id]] = talisman;
+        }
+        
+    }
+    //make activeTalismans into array
+    activeTalismans = Object.keys(activeTalismans).map(x=>activeTalismans[x]);
+
+    //find talisman base stats + rarity counts
+    let rarityCounts = {
+        COMMON: 0,
+        UNCOMMON: 0,
+        RARE: 0,
+        EPIC: 0,
+        LEGENDARY: 0,
+        MYTHIC: 0
+    }
+    let talismanBaseStats = {}
+    for (let talisman of activeTalismans) {
+        rarityCounts[talisman.rarity]++;
+        for (let stat in talisman.baseStats) {
+            if (!talismanBaseStats[stat]) {
+                talismanBaseStats[stat] = talisman.baseStats[stat]
+            } else {
+                talismanBaseStats[stat] += talisman.baseStats[stat]
+            }
+        }
+    }
 }
 
 function staticStatsModifier(stats) {
--- public/js/stats/slayer.js
+++ public/js/stats/slayer.js
@@ -14,9 +14,8 @@ function loadSlayer(profileData) {
         slayerDisplay.classList.add("slayer");
         slayerDisplay.innerHTML = `
         <div class='slayer-header'></div>
-        <div class='slayer-kills'>
-            <div class="slayer-kills-header">${slayerToBoss[slayer] + " x" + Object.keys(profileData.slayer[slayer].boss_kills).reduce((t,x) => t + profileData.slayer[slayer].boss_kills[x],0)}</div>
-        </div>
+        <div class="slayer-kills-header">${slayerToBoss[slayer] + " x" + Object.keys(profileData.slayer[slayer].boss_kills).reduce((t,x) => t + profileData.slayer[slayer].boss_kills[x],0)}</div>
+        <div class='slayer-kills'></div>
         <div class='slayer-bar'>
             <div class='slayer-bar-fill'></div>
             <div class='slayer-bar-text'></div>
@@ -25,19 +24,21 @@ function loadSlayer(profileData) {
         //make slayer header
         slayerDisplay.querySelector(".slayer-header").innerText = slayer.charAt(0).toUpperCase() + slayer.slice(1) + " " + profileData.slayer[slayer].level;
         //make kills grid
-        slayerDisplay.querySelector(".slayer-kills").style.gridTemplateColumns = `repeat(${Object.keys(profileData.slayer[slayer].boss_kills).length},1fr)`;
         for (let tier in profileData.slayer[slayer].boss_kills) {
-            let label = document.createElement("div");
-            label.innerText = "Tier " + tier[tier.length - 1];
-            if (!profileData.slayer[slayer].boss_kills[tier] > 0) label.innerText = "" //don't show uncompleted tiers
-            label.classList.add("slayer-kills-label");
-            slayerDisplay.querySelector(".slayer-kills").appendChild(label)
-        }
-        for (let tier in profileData.slayer[slayer].boss_kills) {
-            let label = document.createElement("div");
-            label.innerText = cleanFormatNumber(profileData.slayer[slayer].boss_kills[tier]);
-            if (!profileData.slayer[slayer].boss_kills[tier] > 0) label.innerText = "" //don't show uncompleted tiers
-            slayerDisplay.querySelector(".slayer-kills").appendChild(label)
+            if (profileData.slayer[slayer].boss_kills[tier] > 0) { //don't show uncompleted tiers
+                let box = document.createElement("div"); //put all the things in box
+
+                let label = document.createElement("div");
+                label.innerText = "Tier " + tier[tier.length - 1];
+                label.classList.add("slayer-kills-label");
+
+                let kills = document.createElement("div");
+                kills.innerText = cleanFormatNumber(profileData.slayer[slayer].boss_kills[tier]);
+
+                box.appendChild(label);
+                box.appendChild(kills);
+                slayerDisplay.querySelector(".slayer-kills").appendChild(box);
+            }
         }
         //make bar
         if (profileData.slayer[slayer].level == profileData.slayer[slayer].maxLevel) {
